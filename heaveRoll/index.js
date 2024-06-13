aa51ddd93a57af1b203814ea1f34f55f809f0191
amaana05 <164677417+amaana05@users.noreply.github.com>
2024-06-13 19:08:29 +0530

    JS missing fix
    trying to figure out the js missing issue

--- heaveRoll/index.js
+++ heaveRoll/index.js
@@ -4,7 +4,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage1 = "dice"+ randomNumber1+".png";
 
-var randomImageSource1 = "heaveRoll/images/"+randomDiceImage1;
+var randomImageSource1 = "/heaveRoll/images/"+randomDiceImage1;
 
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource1);
@@ -17,7 +17,7 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage2 = "dice"+ randomNumber2+".png";
 
-var randomImageSource2 = "heaveRoll/images/"+randomDiceImage2;
+var randomImageSource2 = "/heaveRoll/images/"+randomDiceImage2;
 
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSource2);
