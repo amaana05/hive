a05ab131a47ecae16ffa2f1d306177ed62889c8c
amaana05 <164677417+amaana05@users.noreply.github.com>
2024-06-16 18:25:29 +0530

    Finished the Pokedex
    - minor design changes

---
+++ Pokédex/index.js
@@ -0,0 +1,24 @@
+fetchData();
+
+async function fetchData(){
+
+    try{
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    }
+    catch(error){
+        console.error(error);
+    }
+}
