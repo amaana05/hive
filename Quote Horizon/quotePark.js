059011f3bf1ea156c6a4bb2d009755fefc222a5f
amaana05 <164677417+amaana05@users.noreply.github.com>
2024-07-16 05:18:50 +0530

    added a random quote generator named Quote Horizon
    - used an api to fetch random quotes
    - the background changes based on the time of the day

---
+++ Quote Horizon/quotePark.js
@@ -0,0 +1,56 @@
+        //FETHING THE DATA FROM THE URL
+
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+async function getQuote(){
+    
+    try{
+        const response = await fetch('https://dummyjson.com/quotes/random');
+        //console.log(response);
+
+        if(!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const quoteData = await response.json();
+        //console.log(quoteData);
+
+        quote.textContent = quoteData.quote;
+        author.textContent = `— ${quoteData.author}`;
+
+    }
+    catch(error){
+        console.log(error.message);
+    }
+}
+
+
+        //CHANING THE BACKGROUND IMAGE BASED ON THE TIME OF THE DAY
+
+const body = document.querySelector('body');
+const date = new Date();
+const hour = date.getHours();
+
+console.log(hour);
+
+
+if(hour >= 20 || hour < 5){
+    body.style.backgroundImage = "url('./images/night.png')";
+} 
+else if (hour >=5 || hour < 12) {
+    body.style.backgroundImage = "url('./images/morning.jpg')";
+}
+else if (hour >=12 || hour < 17){
+    body.style.backgroundImage = "url('./images/afternoon.jpg')";
+}
+else{
+    body.style.backgroundImage = "url('./images/evening.jpg')";
+}
+
+
+
+// hour >= 20 || hour < 5 --- night
+// hour >=5 || hour < 12 --- morning
+// hour >=12 || hour < 17 --- afternoon
+// hour >=17 || hour < 20 --- evening
